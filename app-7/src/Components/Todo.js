8641e6459c2a8f36658d1313eafd6d14eb78e9d4
Kimberly Izatt <kimizatt@Jasons-MacBook-Pro.local>
2019-06-19 08:35:49 -0600

    Finished 6 and 7

---
+++ app-7/src/Components/Todo.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class ToDo extends Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.task}</p>
+            </div>
+        )
+    }
+}
+export default ToDo
